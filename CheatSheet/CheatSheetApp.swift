28634ffe094dfce747a3dc4683c6ad9347850c96
Kubilaycan <can.kubilay@outlook.com>
2021-09-27 09:37:35 +0300

    Initial Commit

---
+++ CheatSheet/CheatSheetApp.swift
@@ -0,0 +1,17 @@
+//
+//  CheatSheetApp.swift
+//  CheatSheet
+//
+//  Created by Kubilay on 27.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct CheatSheetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CheatSheet/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CheatSheet
+//
+//  Created by Kubilay on 27.09.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
